ffc7171e4ba99095a65bc73b2b41452d3e7ba098
Alexander Gurevich <guran1996@gmail.com>
2020-09-17 19:23:56 +0300

    fix: use unique strategy names for each created middleware
    fixes #5

--- passport.js
+++ passport.js
@@ -1,11 +1,20 @@
 const passport = require("passport");
 const { Strategy: JwtStrategy, ExtractJwt } = require("passport-jwt");
 
-
-const getPassport = (secretOrKey, useBearerToken, verifyFunction) => {
+const addJwtStrategyToPassport = (secretOrKey, useBearerToken, verifyFunction, name = 'spike-auth-middleware-jwt') => {
     const jwtFromRequest = useBearerToken ? ExtractJwt.fromAuthHeaderAsBearerToken() : ExtractJwt.fromHeader('authorization');
-    return passport.use(new JwtStrategy({ secretOrKey, jwtFromRequest }, verifyFunction));
+    passport.use(name, new JwtStrategy({ secretOrKey, jwtFromRequest }, verifyFunction));
 };
 
+let jwtStrategyIndex = 0;
 
-module.exports = getPassport;
+const getPassportAuthMiddleware = (secretOrKey, useBearerToken, verifyFunction) => {
+    // ensure unique strategy name
+    const strategyName = `spike-auth-middleware-jwt${jwtStrategyIndex++}`;
+
+    addJwtStrategyToPassport(secretOrKey, useBearerToken, verifyFunction, strategyName);
+
+    return passport.authenticate(strategyName, { session: false });
+}
+
+module.exports = getPassportAuthMiddleware;
